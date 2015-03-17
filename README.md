31a475c91e44fb2aa3cff1f6945a623f71324a3f
Michael Saint Clair <ceo.wonu@gmail.com>
2015-03-16 23:36:08 -0400

    Commit the new_options app

---
+++ README.md
@@ -0,0 +1,3 @@
+<header>
+  <h1>New Options</h1>
+    <h3>I used rails -T option to create the new_options app</h3>
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewOptions::Application.config.secret_token = '2c809a68c0f8b4d86ca1e147e355ed2b7700c8631585afa7127898fb9369763beed2a9a35bd72f1808f09082097984653361ca463d102e1e2ddce6377480b5db'
